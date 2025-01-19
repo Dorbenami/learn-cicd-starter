9f87266ce5006a8e9deafbd8fabae609894bb993
Dorbenami <benami.dordor@gmail.com>
2025-01-19 16:04:26 +0200

    added gosec to ci pipeline (fixed bugs)

--- json.go
+++ json.go
@@ -7,8 +7,8 @@ import (
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if code > 499 {
-		log.Printf("Responding with 5XX error: %s", msg)
+	if code >= 500 {
+		log.Printf("Responding with server error. Code: %d, Message: %s", code, msg)
 	}
 	type errorResponse struct {
 		Error string `json:"error"`
@@ -23,9 +23,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError) // 500
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if n, err := w.Write(dat); err != nil {
+		log.Printf("Error writing response: %s (bytes written: %d)", err, n)
+	}
 }
